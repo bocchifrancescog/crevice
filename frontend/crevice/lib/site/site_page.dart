797f270c9228418ec3a4e3079ecadab22d31a975
francesco <bocchifrancescog@gmail.com>
2021-02-14 15:18:07 +0000

    base routing

---
+++ frontend/crevice/lib/site/site_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SitePage extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Site'),
+    );
+  }
+}
